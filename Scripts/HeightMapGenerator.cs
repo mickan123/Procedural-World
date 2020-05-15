b4789989c98fe8075a435080ce3bdda52d5e292b
Mitchell Mickan <mitchellmickan@gmail.com>
2020-05-15 18:11:00 +0930

    Revert sand dunes to old way

--- Scripts/HeightMapGenerator.cs
+++ Scripts/HeightMapGenerator.cs
@@ -74,74 +74,43 @@ public static class HeightMapGenerator {
 		
 		float[,] values = new float[width, height];
 
-		float[,] noiseValues = Noise.GenerateNoiseMap(width,
-													height,
-													noiseSettings.perlinNoiseSettings,
-													sampleCentre,
-													NoiseMapSettings.NoiseType.Simplex,
-													seed);
-
+		
 		int chunkIdxY = (int)sampleCentre.y / meshSettings.meshWorldSize;
 		
-		SandDuneSettings settings = noiseSettings.sandDuneSettings;
+		for (int w = 0; w < noiseSettings.sandDuneSettings.Length; w++) {
 
-		float totalSequenceLength = 0f;
-		for (int i = 0; i < settings.duneWidth.Length; i++) {
-			totalSequenceLength += settings.duneWidth[i];
-			totalSequenceLength += settings.duneGap;
-		}
+			SandDuneSettings settings = noiseSettings.sandDuneSettings[w];
+			float duneHeight = ((2 * settings.sigma * settings.duneWidth) / Mathf.PI) * Mathf.Max(1 - settings.xm, 0.01f);
 
-		for (int i = 0; i < width; i++) {
-			for (int j = 0; j < height; j++) {
-				
-				// Calculate dunewidth to use based on position in sequence
-				float duneOffset = noiseValues[i, j] * settings.maxDuneVariation + settings.duneOffset;
-				float posInSequence = (j + duneOffset + chunkIdxY * (3 - height)) % totalSequenceLength;
-				posInSequence = (posInSequence < 0) ? posInSequence + totalSequenceLength : posInSequence;
+			float[,] noiseValues = Noise.GenerateNoiseMap(width,
+														height,
+														noiseSettings.perlinNoiseSettings,
+														sampleCentre,
+														NoiseMapSettings.NoiseType.Simplex,
+														seed + w);
+			for (int i = 0; i < width; i++) {
+				for (int j = 0; j < height; j++) {
+					float duneLength = settings.duneWidth + settings.duneGap;
+					float duneOffset = noiseValues[i, j] * settings.maxDuneVariation + settings.duneOffset;
+					float x = (j + duneOffset + chunkIdxY * (height - 3)) % duneLength;
+					
+					x = (x < 0) ? x + duneLength : x;
+					x = Mathf.Clamp(x / settings.duneWidth, 0, 1);
+					
+					// Calculate dune height and equation according to Laurent Avenel (Wiwine) Terragen tutorial
+					float side = (x > settings.xm) ? 1f : 0f;
+					float cosTerm = 1f - Mathf.Cos((Mathf.PI / (settings.p * side + 1)) * ((x - side) / (settings.xm - side)));
+					float constant = ((settings.p * side + 1) / 2f);
+					
+					// Calculate height multiplier with range[0, 1], perlin height value must be > duneThreshold or it is clamped to 0 
+					float duneThresholdMultiplier = Mathf.Max(0, noiseValues[i, j] - settings.duneThreshold) / (Mathf.Max(1f - settings.duneThreshold, 0.01f));
 
-				int duneWidthIndex = 0;
-				float curSeqPos = 0f;
-				while (true) {
-					curSeqPos += settings.duneWidth[duneWidthIndex];
-					curSeqPos += settings.duneGap;
-					if (curSeqPos >= posInSequence) {
-						break;
-					}
-					duneWidthIndex++;
+					values[i, j] += (constant * cosTerm) * duneHeight * duneThresholdMultiplier;
 				}
-				float duneWidth = settings.duneWidth[duneWidthIndex];
-				float x = posInSequence - (curSeqPos - duneWidth - settings.duneGap); // X = position along in current dune
-				x = Mathf.Clamp(x / duneWidth, 0, 1);
-				
-				// Calculate dune height and equation according to Laurent Avenel (Wiwine) Terragen tutorial
-				float side = (x > settings.xm) ? 1f : 0f;
-				float cosTerm = 1f - Mathf.Cos((Mathf.PI / (settings.p * side + 1)) * ((x - side) / (settings.xm - side)));
-				float constant = ((settings.p * side + 1) / 2f);
-				
-				// Calculate height multiplier with range[0, 1], perlin height value must be > duneThreshold or it is clamped to 0 
-				float duneThresholdMultiplier = Mathf.Max(0, noiseValues[i, j] - settings.duneThreshold) / (Mathf.Max(1f - settings.duneThreshold, 0.01f));
-
-				// Calculate dune height according to dune width and max repose angle of sand
-				float duneHeight = ((2 * settings.sigma * duneWidth) / Mathf.PI) * Mathf.Max(1 - settings.xm, 0.01f);
-
-				values[i, j] += (constant * cosTerm) * duneHeight * duneThresholdMultiplier;
 			}
+
 		}
 
 		return values;
-	}
-
-	public static float CalculateDunePos(SandDuneSettings settings, float duneWidth,  int height, int chunkIdxY, int j, float noiseVal) {
-		// Calculate where along sand dune we are:
-		// [0, xm] = on the curve upwards to peak 
-		// [xm, 1) = on the downwards curve
-		// [1, inf] = gap between dunes
-		// Random offset to generate wavey peaks
-		float duneOffset = noiseVal * settings.maxDuneVariation + settings.duneOffset; // height - 1 is max value for height, subtract another 
-		float duneLength = duneWidth + settings.duneGap;
-		float x = (j + duneOffset + chunkIdxY * (3 - height)) % duneLength; 
-		x = x < 0 ? x + duneLength : x;
-		x = Mathf.Clamp(x / duneWidth, 0, 1);
-		return x;
 	}
 }
--- TODO.md
+++ TODO.md
@@ -1,10 +1,25 @@
 [] Better biome selection (e.g. ensure values don't overlap)
 [] Look at biome blending again (should be ok with proper erosion)
 [] Erosion chunk border blending
-[] Collisions between terrainobjects
-[] Threadpool
+
+[] Terrain objects
+    [x] Simple poisson disc sampling
+    [x] Disc sampling with varying radius
+    [] Collisions between terrain objects
+    [] Aligning bottom of terrain object with ground
 [] Sand Dunes
+    [x] Simple sand dune algorithm
+    [] 
 [] Road Generation
+    [x] A* road path finding
+    [x] Road generation for single chunk
+    [] Road generation based on biome
+    [] Cross chunk roads
 [] River Generation
+    [] River path finding single chunk
+    [] River carve into ground
+    [] Decentish water shader
 [] Continent Generation
+    []
 [] Procedurally generated trees (L-system etc)
+[] Threadpool
