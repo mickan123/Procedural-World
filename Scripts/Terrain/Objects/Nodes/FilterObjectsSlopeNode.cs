6ac531fbaaf293dace1f652d724754a6f1449640
Mitchell Mickan <mitchellmickan@gmail.com>
2021-02-25 21:36:56 +1030

    Optimise slope filtering

--- Scripts/Terrain/Objects/Nodes/FilterObjectsSlopeNode.cs
+++ Scripts/Terrain/Objects/Nodes/FilterObjectsSlopeNode.cs
@@ -30,10 +30,49 @@ public class FilterObjectsSlopeNode : BiomeGraphNode
         {
             return null;
         }
+
+        // Generate slope at every point
+        int maxIndex = positionData.heightMap.GetLength(0) - 1;
+        float[,] slopeMap = new float[positionData.heightMap.GetLength(0), positionData.heightMap.GetLength(1)];
+        for (int x = 0; x < slopeMap.GetLength(0); x++)
+        {
+            for (int y = 0; y < slopeMap.GetLength(1); y++)
+            {
+                float height = positionData.heightMap[x, y];
+
+                // Compute the differentials by stepping over 1 in both directions.
+                float dx = positionData.heightMap[Mathf.Min(x + 1, maxIndex), y] - height;
+
+                float dy = positionData.heightMap[x, Mathf.Min(y + 1, maxIndex)] - height;
+
+                float dMax = Mathf.Max(Mathf.Abs(dx), Mathf.Abs(dy));
+                slopeMap[x, y] = Mathf.Rad2Deg * Mathf.Atan2(
+                    dMax, 
+                    1
+                );;
+            }
+        }
+
+        // Iterate over points and filter those that don't match slope criteria
         for (int i = 0; i < positionData.positions.Count; i++)
         {
             Vector3 curPoint = positionData.positions.positions[i];
-            float angle = Common.CalculateAngle(curPoint.x, curPoint.z, positionData.heightMap);
+
+            float xIn = curPoint.x;
+            float yIn = curPoint.z;
+
+            int coordX = (int)xIn;
+            int coordZ = (int)yIn;
+
+            // Calculate offset inside the cell (0,0) = at NW node, (1,1) = at SE node
+            float x = xIn - coordX;
+            float y = yIn - coordZ;
+
+            float xSlope = x * slopeMap[coordX, coordZ] + (1f - x) * slopeMap[Mathf.Min(coordX + 1, maxIndex), coordZ];
+            float ySlope = y * slopeMap[coordX, coordZ] + (1f - y) * slopeMap[coordX, Mathf.Min(coordZ + 1, maxIndex)];
+
+            float angle = (xSlope + ySlope) / 2f;
+
             if (angle > maxAngle || angle < minAngle)
             {
                 positionData.positions.filtered[i] = true;
