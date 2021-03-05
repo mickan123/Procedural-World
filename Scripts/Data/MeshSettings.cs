e7d6abee1c3dc1ac1d8de47be2d9b384b4b1ecb4
Mitchell Mickan <mitchellmickan@gmail.com>
2021-03-05 10:51:56 +1030

    Remove flat shading option

--- Scripts/Data/MeshSettings.cs
+++ Scripts/Data/MeshSettings.cs
@@ -7,15 +7,12 @@ public class MeshSettings : ScriptableObject
 {
     public const int numSupportedLODs = 5;
     public const int numSupportedChunkSizes = 11;
-    public const int numSupportedFlatshadedChunkSizes = 3;
     public static readonly int[] supportedChunkSizes = { 48, 72, 96, 120, 144, 168, 192, 216, 240, 480, 960 };
 
     public int meshScale = 1;
 
     [Range(0, numSupportedChunkSizes - 1)]
     public int chunkSizeIndex;
-    [Range(0, numSupportedFlatshadedChunkSizes - 1)]
-    public int flatshadedChunkSizeIndex;
 
 #if UNITY_EDITOR
 
